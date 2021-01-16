8e6811912e17fdeab73f46e087e77ea694df54e8
Ngoan Ho <ngoanh2n@gmail.com>
2021-01-16 21:24:27 +0700

    Add H2nException

---
+++ src/main/java/io/github/ngoanh2n/H2nException.java
@@ -0,0 +1,25 @@
+package io.github.ngoanh2n;
+
+/**
+ * Base runtime exception for io.github.ngoanh2n
+ *
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @version 1.0.0
+ * @since 2020-01-06
+ */
+public class H2nException extends RuntimeException {
+
+    private static final long serialVersionUID = 635198548542132913L;
+
+    public H2nException(String message) {
+        super(message);
+    }
+
+    public H2nException(Throwable cause) {
+        super(cause);
+    }
+
+    public H2nException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
