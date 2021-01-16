65e85d8fb48aa0d4b672bfcd273b16903b765be9
Ngoan Ho <ngoanh2n@gmail.com>
2021-01-16 21:38:54 +0700

    Add Commons

---
+++ src/main/java/io/github/ngoanh2n/commons/Commons.java
@@ -0,0 +1,19 @@
+package io.github.ngoanh2n.commons;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Common helpers and utilities
+ *
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @version 1.0.0
+ * @since 2020-01-16
+ */
+public class Commons {
+
+    public static String timeStamp() {
+        //noinspection SpellCheckingInspection
+        return new SimpleDateFormat("yyyyMMdd.HHmmss.SSS").format(new Date());
+    }
+}
