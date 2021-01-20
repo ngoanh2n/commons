dab6ae514bf01d1eb7c553898eb688678fa091b2
Ngoan Ho <ngoanh2n@gmail.com>
2021-01-20 22:15:22 +0700

    Rename to RuntimeError

--- src/main/java/io/github/ngoanh2n/RuntimeError.java
+++ src/main/java/io/github/ngoanh2n/RuntimeError.java
@@ -7,19 +7,19 @@ package io.github.ngoanh2n;
  * @version 1.0.0
  * @since 2021-01-06
  */
-public class NgRuntimeException extends RuntimeException {
+public class RuntimeError extends RuntimeException {
 
     private static final long serialVersionUID = 635198548542132913L;
 
-    public NgRuntimeException(String message) {
+    public RuntimeError(String message) {
         super(message);
     }
 
-    public NgRuntimeException(Throwable cause) {
+    public RuntimeError(Throwable cause) {
         super(cause);
     }
 
-    public NgRuntimeException(String message, Throwable cause) {
+    public RuntimeError(String message, Throwable cause) {
         super(message, cause);
     }
 }
