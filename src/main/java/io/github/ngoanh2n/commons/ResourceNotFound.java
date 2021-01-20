eb8a5805516d1f00eeba36f31ccd1cdda812ac54
Ngoan Ho <ngoanh2n@gmail.com>
2021-01-20 22:16:00 +0700

    Use RuntimeError

--- src/main/java/io/github/ngoanh2n/commons/ResourceNotFound.java
+++ src/main/java/io/github/ngoanh2n/commons/ResourceNotFound.java
@@ -1,6 +1,6 @@
 package io.github.ngoanh2n.commons;
 
-import io.github.ngoanh2n.NgoanException;
+import io.github.ngoanh2n.RuntimeError;
 
 /**
  * Throws when could not find Java Resources
@@ -9,7 +9,7 @@ import io.github.ngoanh2n.NgoanException;
  * @version 1.0.0
  * @since 2021-01-16
  */
-public class ResourceNotFound extends NgoanException {
+public class ResourceNotFound extends RuntimeError {
 
     public ResourceNotFound(String resourceName) {
         super("Resource not found: " + resourceName);
