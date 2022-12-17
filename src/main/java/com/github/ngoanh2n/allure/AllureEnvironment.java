c390470cf732802c456450dbf72a1578d2f817da
ngoanh2n <ngoanh2n@gmail.com>
2022-12-17 13:32:12 +0700

    Write environment.properties to results dir

--- src/main/java/com/github/ngoanh2n/allure/AllureEnvironment.java
+++ src/main/java/com/github/ngoanh2n/allure/AllureEnvironment.java
@@ -1,8 +1,11 @@
 package com.github.ngoanh2n.allure;
 
 import com.github.ngoanh2n.Commons;
+import io.qameta.allure.util.PropertiesUtils;
 
+import java.io.File;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Properties;
 
@@ -12,14 +15,25 @@ import java.util.Properties;
  * @since 2022-12-17
  */
 public class AllureEnvironment {
-    public static void write(Map<String, String> map, Path resultsDir) {
+    public static void write(Map<String, String> map) {
         Properties props = new Properties();
         props.putAll(map);
+        write(props);
+    }
+
+    public static void write(Properties props) {
+        Path resultsDir = getResultsDir();
         write(props, resultsDir);
     }
 
     public static void write(Properties props, Path resultsDir) {
         String name = "environment.properties";
-        Commons.writeProps(props, resultsDir.resolve(name).toFile());
+        File file = resultsDir.resolve(name).toFile();
+        Commons.writeProps(props, file);
+    }
+
+    private static Path getResultsDir() {
+        Properties props = PropertiesUtils.loadAllureProperties();
+        return Paths.get(props.getProperty("allure.results.directory", "allure-results"));
     }
 }
