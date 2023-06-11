da7e0959b0c43b726c4c56a966a7ca4707e32424
ngoanh2n <ngoanh2n@gmail.com>
2023-06-11 09:15:08 +0700

    Update EnabledIfPropertyTest

--- commons-junit5/src/test/java/com/github/ngoanh2n/EnabledIfPropertyTest.java
+++ commons-junit5/src/test/java/com/github/ngoanh2n/EnabledIfPropertyTest.java
@@ -1,37 +1,18 @@
 package com.github.ngoanh2n;
 
 import org.junit.jupiter.api.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author ngoanh2n
  */
 public class EnabledIfPropertyTest {
-    private static final String NAME = "prop";
+    private static final String NAME = "property";
     private static final String VALUE = "ngoanh2n";
     private static final Property<String> PROPERTY = Property.ofString(NAME);
 
     static final class EnabledIfPropertyBTest {
-        private static final Logger logger = LoggerFactory.getLogger(EnabledIfPropertyBTest.class);
-
         @BeforeAll
         static void beforeAll() {
-            logger.debug("" + System.getProperty(NAME));
-            logger.debug("" + PROPERTY.getValue());
-            logger.debug("" + Property.ofString(NAME).getValue());
-
-            Assertions.assertNull(System.getProperty(NAME));
-            Assertions.assertNull(PROPERTY.getValue());
-            Assertions.assertNull(Property.ofString(NAME).getValue());
-        }
-
-        @AfterAll
-        static void afterAll() {
-            logger.debug("" + System.getProperty(NAME));
-            logger.debug("" + PROPERTY.getValue());
-            logger.debug("" + Property.ofString(NAME).getValue());
-
             Assertions.assertNull(System.getProperty(NAME));
             Assertions.assertNull(PROPERTY.getValue());
             Assertions.assertNull(Property.ofString(NAME).getValue());
@@ -39,10 +20,6 @@ public class EnabledIfPropertyTest {
 
         @BeforeEach
         void beforeEach() {
-            logger.debug(System.getProperty(NAME));
-            logger.debug(PROPERTY.getValue());
-            logger.debug(Property.ofString(NAME).getValue());
-
             Assertions.assertEquals(VALUE, System.getProperty(NAME));
             Assertions.assertEquals(VALUE, PROPERTY.getValue());
             Assertions.assertEquals(VALUE, Property.ofString(NAME).getValue());
@@ -59,26 +36,6 @@ public class EnabledIfPropertyTest {
 
         @AfterEach
         void afterEach() {
-            logger.debug(System.getProperty(NAME));
-            logger.debug(PROPERTY.getValue());
-            logger.debug(Property.ofString(NAME).getValue());
-
-            Assertions.assertEquals(VALUE, System.getProperty(NAME));
-            Assertions.assertEquals(VALUE, PROPERTY.getValue());
-            Assertions.assertEquals(VALUE, Property.ofString(NAME).getValue());
-        }
-    }
-
-    @SetProperty(name = NAME, value = VALUE)
-    static final class EnabledIfPropertyATest {
-        private static final Logger logger = LoggerFactory.getLogger(EnabledIfPropertyATest.class);
-
-        @BeforeAll
-        static void beforeAll() {
-            logger.debug(System.getProperty(NAME));
-            logger.debug(PROPERTY.getValue());
-            logger.debug(Property.ofString(NAME).getValue());
-
             Assertions.assertEquals(VALUE, System.getProperty(NAME));
             Assertions.assertEquals(VALUE, PROPERTY.getValue());
             Assertions.assertEquals(VALUE, Property.ofString(NAME).getValue());
@@ -86,10 +43,16 @@ public class EnabledIfPropertyTest {
 
         @AfterAll
         static void afterAll() {
-            logger.debug(System.getProperty(NAME));
-            logger.debug(PROPERTY.getValue());
-            logger.debug(Property.ofString(NAME).getValue());
+            Assertions.assertNull(System.getProperty(NAME));
+            Assertions.assertNull(PROPERTY.getValue());
+            Assertions.assertNull(Property.ofString(NAME).getValue());
+        }
+    }
 
+    @SetProperty(name = NAME, value = VALUE)
+    static final class EnabledIfPropertyATest {
+        @BeforeAll
+        static void beforeAll() {
             Assertions.assertEquals(VALUE, System.getProperty(NAME));
             Assertions.assertEquals(VALUE, PROPERTY.getValue());
             Assertions.assertEquals(VALUE, Property.ofString(NAME).getValue());
@@ -97,10 +60,6 @@ public class EnabledIfPropertyTest {
 
         @BeforeEach
         void beforeEach() {
-            logger.debug(System.getProperty(NAME));
-            logger.debug(PROPERTY.getValue());
-            logger.debug(Property.ofString(NAME).getValue());
-
             Assertions.assertEquals(VALUE, System.getProperty(NAME));
             Assertions.assertEquals(VALUE, PROPERTY.getValue());
             Assertions.assertEquals(VALUE, Property.ofString(NAME).getValue());
@@ -116,10 +75,13 @@ public class EnabledIfPropertyTest {
 
         @AfterEach
         void afterEach() {
-            logger.debug(System.getProperty(NAME));
-            logger.debug(PROPERTY.getValue());
-            logger.debug(Property.ofString(NAME).getValue());
+            Assertions.assertEquals(VALUE, System.getProperty(NAME));
+            Assertions.assertEquals(VALUE, PROPERTY.getValue());
+            Assertions.assertEquals(VALUE, Property.ofString(NAME).getValue());
+        }
 
+        @AfterAll
+        static void afterAll() {
             Assertions.assertEquals(VALUE, System.getProperty(NAME));
             Assertions.assertEquals(VALUE, PROPERTY.getValue());
             Assertions.assertEquals(VALUE, Property.ofString(NAME).getValue());
