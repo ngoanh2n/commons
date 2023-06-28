9016b066f21fa8d4f4491feeddd149afb66a839a
ngoanh2n <ngoanh2n@gmail.com>
2023-06-28 23:29:55 +0700

    Modify PropertiesFileTest

--- commons/src/test/java/com/github/ngoanh2n/PropertiesFileTest.java
+++ commons/src/test/java/com/github/ngoanh2n/PropertiesFileTest.java
@@ -1,146 +1,177 @@
 package com.github.ngoanh2n;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 /**
+ * System property (A), Properties file (B), Default value (C)
+ *
  * @author ngoanh2n
  */
 public class PropertiesFileTest {
-    private static final String PROP_NAME1 = "PropFile.PropName1";
-    private static final String PROP_NAME2 = "PropFile.PropName2";
-    private static final String PROP_NAME3 = "PropFile.PropName3";
-    private static final String PROP_NAME4 = "PropFile.PropName4";
+    private static final String PROP_NAME1 = "PropertiesFile.PropertyName1";
+    private static final String PROP_NAME2 = "PropertiesFile.PropertyName2";
+    private static final String PROP_NAME3 = "PropertiesFile.PropertyName3";
+    private static final String PROP_NAME4 = "PropertiesFile.PropertyName4";
 
-    private static final String PROP_VALUE0 = "PropValue0";
-    private static final String PROP_VALUE1 = "PropValue1";
-    private static final String PROP_VALUE2 = "PropValue2";
-    private static final String PROP_VALUE3 = "PropValue3";
+    private static final String PROP_VALUE1 = "PropertyValue1";
+    private static final String PROP_VALUE2 = "PropertyValue2";
+    private static final String PROP_VALUE3 = "PropertyValue3";
+    private static final String PROP_VALUE4 = "PropertyValue4";
 
-    /*
-     * JVM System Property: No
-     * Properties file: No
-     * Property<?> object: Yes
-     * */
-    @Test
-    void test1() {
-        PropertiesFile propertiesFile = new PropertiesFile("File1.properties");
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class WithoutFile {
+        PropertiesFile propertiesFile = new PropertiesFile("com/github/ngoanh2n/PropFileTest/File1.properties");
 
-        /*
-         * Value in Properties file: NO-FILE
-         * Value in Property<?> object: NULL
-         * => Value: NULL
-         * */
-        Property<String> property1 = new Property<>(PROP_NAME1, String.class);
-        Assertions.assertNull(propertiesFile.getProperty(property1));
+        @Test
+        @DisplayName("{A: NO, B: NO, C: NO}")
+        void test1() {
+            Property<String> property1 = new Property<>(PROP_NAME1, String.class);
+            Assertions.assertNull(propertiesFile.getProperty(property1));
+        }
 
-        /*
-         * Value in Properties file: NO-FILE
-         * Value in Property<?> object: PropValue2
-         * => Value: PropValue2
-         * */
-        Property<String> property2 = new Property<>(PROP_NAME2, String.class, PROP_VALUE2);
-        Assertions.assertEquals(PROP_VALUE2, propertiesFile.getProperty(property2));
+        @Test
+        @DisplayName("{A: NO, B: NO, C: YES}")
+        void test2() {
+            Property<String> property2 = new Property<>(PROP_NAME2, String.class, PROP_VALUE2);
+            Assertions.assertEquals(PROP_VALUE2, propertiesFile.getProperty(property2));
+        }
+
+        @Test
+        @DisplayName("{A: YES, B: NO, C: NO}")
+        void test3() {
+            System.setProperty(PROP_NAME3, PROP_VALUE3);
+            Property<String> property3 = new Property<>(PROP_NAME3, String.class);
+            Assertions.assertEquals(PROP_VALUE3, propertiesFile.getProperty(property3));
+        }
+
+        @Test
+        @DisplayName("{A: YES, B: NO, C: YES}")
+        void test4() {
+            System.setProperty(PROP_NAME4, PROP_VALUE4);
+            Property<String> property4 = new Property<>(PROP_NAME4, String.class, PROP_VALUE4);
+            Assertions.assertEquals(PROP_VALUE4, propertiesFile.getProperty(property4));
+        }
+
+        @AfterAll
+        void cleanup() {
+            System.clearProperty(PROP_NAME1);
+            System.clearProperty(PROP_NAME2);
+            System.clearProperty(PROP_NAME3);
+            System.clearProperty(PROP_NAME4);
+        }
     }
 
-    /*
-     * JVM System Property: No
-     * Properties file: Yes
-     * Property<?> object: Yes
-     * */
-    @Test
-    void test2() {
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class WithFile {
         PropertiesFile propertiesFile = new PropertiesFile("com/github/ngoanh2n/PropFileTest/File2.properties");
 
-        /*
-         * Value in JVM System Property: NULL
-         * Value in Properties file: PropValue1
-         * Value in Property<?> object: NULL
-         * => Value: PropValue1
-         * */
-        Property<String> property1 = new Property<>(PROP_NAME1, String.class);
-        Assertions.assertEquals(PROP_VALUE1, propertiesFile.getProperty(property1));
+        @Test
+        @DisplayName("{A: NO, B: YES, C: NO}")
+        void test1() {
+            Property<String> property1 = new Property<>(PROP_NAME1, String.class);
+            Assertions.assertEquals(PROP_VALUE1, propertiesFile.getProperty(property1));
+        }
 
-        /*
-         * Value in JVM System Property: NULL
-         * Value in Properties file: PropValue2
-         * Value in Property<?> object: PropValue0
-         * => Value: PropValue2
-         * */
-        Property<String> property2 = new Property<>(PROP_NAME2, String.class, PROP_VALUE0);
-        Assertions.assertEquals(PROP_VALUE2, propertiesFile.getProperty(property2));
+        @Test
+        @DisplayName("{A: NO, B: YES, C: YES}")
+        void test2() {
+            Property<String> property2 = new Property<>(PROP_NAME2, String.class, PROP_VALUE2);
+            Assertions.assertEquals(PROP_VALUE2, propertiesFile.getProperty(property2));
+        }
 
-        /*
-         * Value in JVM System Property: NULL
-         * Value in Properties file: NULL
-         * Value in Property<?> object: PropValue3
-         * => Value: PropValue3
-         * */
-        Property<String> property3 = new Property<>(PROP_NAME3, String.class, PROP_VALUE3);
-        Assertions.assertEquals(PROP_VALUE3, propertiesFile.getProperty(property3));
+        @Test
+        @DisplayName("{A: YES, B: YES, C: NO}")
+        void test3() {
+            System.setProperty(PROP_NAME3, PROP_VALUE3);
+            Property<String> property3 = new Property<>(PROP_NAME3, String.class);
+            Assertions.assertEquals(PROP_VALUE3, propertiesFile.getProperty(property3));
+        }
 
-        /*
-         * Value in JVM System Property: NULL
-         * Value in Properties file: NULL
-         * Value in Property<?> object: NULL
-         * => Value: NULL
-         * */
-        Property<String> property4 = new Property<>(PROP_NAME4, String.class);
-        Assertions.assertNull(propertiesFile.getProperty(property4));
+        @Test
+        @DisplayName("{A: YES, B: YES, C: YES}")
+        void test4() {
+            System.setProperty(PROP_NAME4, PROP_VALUE4);
+            Property<String> property4 = new Property<>(PROP_NAME4, String.class, PROP_VALUE4);
+            Assertions.assertEquals(PROP_VALUE4, propertiesFile.getProperty(property4));
+        }
+
+        @AfterAll
+        void cleanup() {
+            System.clearProperty(PROP_NAME1);
+            System.clearProperty(PROP_NAME2);
+            System.clearProperty(PROP_NAME3);
+            System.clearProperty(PROP_NAME4);
+        }
     }
 
-    /*
-     * JVM System Property: Yes
-     * Properties file: Yes
-     * Property<?> object: Yes
-     * */
-    @Test
-    void test3() {
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class WithFileHasEnum {
         PropertiesFile propertiesFile = new PropertiesFile("com/github/ngoanh2n/PropFileTest/File3.properties");
 
-        /*
-         * Value in JVM System Property: PropValue0
-         * Value in Properties file: NULL
-         * Value in Property<?> object: NULL
-         * => Value: PropValue0
-         * */
-        System.setProperty(PROP_NAME1, PROP_VALUE0);
-        Property<String> property1 = new Property<>(PROP_NAME1, String.class);
-        Assertions.assertEquals(PROP_VALUE0, propertiesFile.getProperty(property1));
+        @Test
+        @DisplayName("{A: NO, B: YES, C: NO}")
+        void test1() {
+            System.setProperty(PROP_NAME1, TestEnum.ENUM2.name());
+            Property<TestEnum> property1 = new Property<>(PROP_NAME1, TestEnum.class, TestEnum.ENUM1);
+            Assertions.assertEquals(TestEnum.ENUM2, propertiesFile.getProperty(property1));
+        }
 
-        /*
-         * Value in JVM System Property: PropValue0
-         * Value in Properties file: PropValue2
-         * Value in Property<?> object: NULL
-         * => Value: PropValue0
-         * */
-        System.setProperty(PROP_NAME2, PROP_VALUE0);
-        Property<String> property2 = new Property<>(PROP_NAME2, String.class, PROP_VALUE2);
-        Assertions.assertEquals(PROP_VALUE0, propertiesFile.getProperty(property2));
+        @Test
+        @DisplayName("{A: NO, B: YES, C: YES}")
+        void test2() {
+            Property<TestEnum> property2 = new Property<>(PROP_NAME2, TestEnum.class, TestEnum.ENUM1);
+            Assertions.assertEquals(TestEnum.ENUM2, propertiesFile.getProperty(property2));
+        }
 
-        /*
-         * Value in JVM System Property: PropValue0
-         * Value in Properties file: PropValue3
-         * Value in Property<?> object: PropValue3
-         * => Value: PropValue0
-         * */
-        System.setProperty(PROP_NAME3, PROP_VALUE0);
-        Property<String> property3 = new Property<>(PROP_NAME3, String.class, PROP_VALUE3);
-        Assertions.assertEquals(PROP_VALUE0, propertiesFile.getProperty(property3));
+        @AfterAll
+        void cleanup() {
+            System.clearProperty(PROP_NAME1);
+            System.clearProperty(PROP_NAME2);
+        }
+    }
 
-        /*
-         * Value in JVM System Property: ENUM0
-         * Value in Properties file: ENUM1
-         * Value in Property<?> object: ENUM2
-         * => Value: ENUM0
-         * */
-        System.setProperty(PROP_NAME4, TestEnum.ENUM0.name());
-        Property<TestEnum> property4 = new Property<>(PROP_NAME4, TestEnum.class, TestEnum.ENUM2);
-        Assertions.assertEquals(TestEnum.ENUM0, propertiesFile.getProperty(property4));
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class WithFileReAssignValue {
+        PropertiesFile propertiesFile = new PropertiesFile("com/github/ngoanh2n/PropFileTest/File4.properties");
+
+        @Test
+        @DisplayName("{A: NO, B: YES, C: YES}")
+        void test2() {
+            Property<String> property2 = new Property<>(PROP_NAME2, String.class, PROP_VALUE2);
+            property2.setValue(null);
+            Assertions.assertNull(propertiesFile.getProperty(property2));
+
+            property2 = new Property<>(PROP_NAME2, String.class, PROP_VALUE2);
+            property2.setValue(PROP_VALUE1);
+            Assertions.assertEquals(PROP_VALUE1, propertiesFile.getProperty(property2));
+        }
+
+        @Test
+        @DisplayName("{A: YES, B: YES, C: YES}")
+        void test4() {
+            System.setProperty(PROP_NAME4, PROP_VALUE4);
+            Property<String> property4 = new Property<>(PROP_NAME1, String.class, PROP_VALUE1);
+            property4.setValue(null);
+            Assertions.assertNull(propertiesFile.getProperty(property4));
+
+            property4 = new Property<>(PROP_NAME1, String.class, PROP_VALUE1);
+            property4.setValue(PROP_VALUE3);
+            Assertions.assertEquals(PROP_VALUE3, propertiesFile.getProperty(property4));
+        }
+
+        @AfterAll
+        void cleanup() {
+            System.clearProperty(PROP_NAME2);
+            System.clearProperty(PROP_NAME4);
+        }
     }
 
     enum TestEnum {
-        ENUM0, ENUM1, ENUM2;
+        ENUM1, ENUM2;
     }
 }
 
